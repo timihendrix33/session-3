48b9ce6069cafa0ee6c6843c06eba5bab2304c63
Tim Stevenson <t.stevenson33@gmail.com>
2017-02-24 17:39:42 -0500

    writing to mLab

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-const express = require('express') 
-// require the npm library
+const express = require('express')
 const bodyParser= require('body-parser')
-const MongoClient = require('mongodb').MongoClient
-const app = express() 
-// create a var for the app to be built using express
-// app is the global variable namespace for the program we are building
+const app = express()
 const port = 9000
-
-//app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
+const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb://tstevenson33:dumbpass1234@ds145649.mlab.com:45649/bcl', (err, database) => {
+MongoClient.connect('mongodb://tstevenson33:dumbpass1234@ds161209.mlab.com:61209/bcl-2', (err, database) => {
    if (err) return console.log(err)
     db = database
   app.listen(port, () => {
@@ -22,17 +17,13 @@ app.get('/', (req, res) =>
 	res.sendFile(__dirname + '/index.html')
 	) 
 
-// app.post('/entries', (req, res) =>
-// 	console.log(req.body)
-// 	)
-
 app.post('/entries', (req, res) => {
-  db.collection('entries').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
+	db.collection('entries').save(req.body, (err, result) => {
+		if (err) return console.log(err)
+			console.log('saved to database')
+		res.redirect('/')
+	})
+})	
 
 app.get('/entry/:name?', function(req, res){
   let name = req.params.name
@@ -40,10 +31,6 @@ app.get('/entry/:name?', function(req, res){
     <h1>${name}</h1>
     <p>Commentary on ${name} will go here.</p>
     `)
-})
-
-app.listen(port, function () {
-  console.log(`Listening on port ${port}!`)
 })
 
 app.get('*', function(req, res){
